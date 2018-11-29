1bc781f2bd7b45f42dc7aa0afdf2cbb610725f14
jyansol <hahn0406@gmail.com>
2018-11-29 12:43:47 +0900

    주소표시줄과 상품 정보 화면 연동

--- src/containers/ProductDetail.js
+++ src/containers/ProductDetail.js
@@ -3,6 +3,10 @@ import ProductDetailView from '../components/ProductDetailView';
 import api from '../api';
 
 export default class ProductDetail extends Component {
+  static defaultProps = {
+    //표시해주어야하는 상품의 id
+    productId: null,
+  };
   constructor(props) {
     super(props);
 
@@ -19,7 +23,8 @@ export default class ProductDetail extends Component {
   }
 
   async componentDidMount() {
-    const { data: products } = await api.get('/products/1');
+    const { productId } = this.props;
+    const { data: products } = await api.get(`/products/${productId}`);
     this.setState({
       ...products,
       loading: false,
