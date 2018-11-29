ce0c6c1daab3945b0290b4629469161cfea86542
jyansol <hahn0406@gmail.com>
2018-11-29 11:34:28 +0900

    ProductListView 작성

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import ProductList from '../containers/ProductList';
 
 export default class HomePage extends Component {
   render() {
     return (
       <div>
-        <h1>Home</h1>
-        <p>API_URL={process.env.REACT_APP_API_URL}</p>
+        <h1>Homee</h1>
+        <ProductList />
+        {/* <p>API_URL={process.env.REACT_APP_API_URL}</p> */}
       </div>
     );
   }
